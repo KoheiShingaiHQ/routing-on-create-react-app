3a5c60450d6336eac82300c66c07a3c162ecfa96
koheishingai <koheishingai+HQ@gmail.com>
2017-09-04 00:15:39 +0900

    Customizing Create React App : Redux

--- src/index.js
+++ src/index.js
@@ -1,5 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import { ConnectedRouter } from 'react-router-redux';
+import store, { history } from './store';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
